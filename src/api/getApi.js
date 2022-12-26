53e7af0e9cfade9356485d2269da72cf99b981e2
kouki-Kubota <kubou1115@outlook.jp>
2022-12-26 16:08:00 +0900

    create get News API

--- src/api/getApi.js
+++ src/api/getApi.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
-const key = '540c0a2d08264331b2101b7c7169ed92';
+// const key = '540c0a2d08264331b2101b7c7169ed92';
+// const url = 'http://localhost:9000/getApi?category='
 
 export const changeCategory = async (category) => {
-  const res = await axios.get('https://newsapi.org/v2/top-headlines?country=jp&category=' + category +  '&apiKey='+ key)
+  const res = await axios.get('/api/getApi?category=' + category)
   
   const articles = res.data.articles;
   return articles
