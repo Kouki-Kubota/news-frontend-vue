08336741bf48506456d2d024d2cabb4be8cc97af
kouki-Kubota <kubou1115@outlook.jp>
2022-12-26 17:43:42 +0900

    connect scala-backend

--- src/api/getApi.js
+++ src/api/getApi.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
-// const key = '540c0a2d08264331b2101b7c7169ed92';
-// const url = 'http://localhost:9000/getApi?category='
+
 
 export const changeCategory = async (category) => {
-  const res = await axios.get('/api/getApi?category=' + category)
+  const res = await axios.get('/api/getNewsApiCategory?category=' + category)
   
   const articles = res.data.articles;
   return articles
